79d84a9f2b3110bc028fd286d7f5781747b922a0
VKKan2002 <venkatakrishnakandalai@gmail.com>
2025-02-20 22:18:33 -0500

    Changes

--- .ipynb_checkpoints/analysis1-checkpoint.py
+++ .ipynb_checkpoints/analysis1-checkpoint.py
@@ -2,8 +2,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from pathlib import Path
-
 
 # Load data from a CSV file dumped in the repo
 data = pd.read_csv('data.csv')
